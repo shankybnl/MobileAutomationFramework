86bef416f3717cb5d8b92903d96c0f584d8b5959
shivam198smart <shivam198smart@gmail.com>
2021-10-30 00:41:49 +0530

    Updated and renamed the file to ExtentReportSetup

--- src/main/java/UITestFramework/retryLogic/ExtentReportSetup.java
+++ src/main/java/UITestFramework/retryLogic/ExtentReportSetup.java
@@ -5,20 +5,17 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
  
 
-public class ExtentReportR 
+public class ExtentReportSetup 
 {
     static ExtentReports extent;
     public static ExtentReports extentReportGenerator()
     {
-    String path= System.getProperty("user.dir") + "\\report\\result.html";     
+    String path= System.getProperty("user.dir") + File.separator + "target" + File.separator + "report" + File.separator + "result.html";    
     ExtentSparkReporter reporter = new ExtentSparkReporter(path);
     reporter.config().setReportName(""); //Give report name
     extent= new ExtentReports();
     extent.attachReporter(reporter);
     extent.setSystemInfo(""); //Set system info whatever you want to show in report
     return extent;
-
- 
-
 }
 }
